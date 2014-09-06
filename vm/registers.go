236f38395d9ee4b83afc712140eefc676ccb8429
Ian Kronquist <iankronquist@teleport.com>
2014-09-06 16:20:11 -0700

    Define the registers for the VM

---
+++ vm/registers.go
@@ -0,0 +1,11 @@
+package vm
+
+const (
+	A = 4  // 8 bit register
+	B = 2  // 8 bit register
+	D = 1  // 16 bit register encompassing A & B
+	X = 16 // 16 bit register
+	Y = 8  // 16 bit register
+)
+
+
